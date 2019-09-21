8ec811220d64a3f634ecbb6c3a75b2bc9c80c547
John Reyes <john.jreye@gmail.com>
2019-09-21 12:10:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
     oxford_comma = ["kiwi"].join
 end
 
-def oxford_end(array, array_1)
+def oxford_end(element1, element2)
   oxford_end = ["kiwi", "durian",].join(" and ")
 end
